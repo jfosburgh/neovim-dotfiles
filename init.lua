a6a636494fbbdc425d76f5dbe8d0101bdce015ef
James Fosburgh <jwfosburgh@gmail.com>
2024-01-08 15:40:03 -0500

    Added files

---
+++ init.lua
@@ -0,0 +1,192 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Example using a list of specs with the default options
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup("plugins")
+
+local lsp_zero = require("lsp-zero")
+
+lsp_zero.on_attach(function(client, bufnr)
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
+end)
+
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	handlers = {
+		lsp_zero.default_setup,
+	},
+})
+
+---
+-- Autocompletion config
+---
+local cmp = require("cmp")
+local cmp_action = lsp_zero.cmp_action()
+
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		-- `Enter` key to confirm completion
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+
+		-- Ctrl+Space to trigger completion menu
+		["<C-Space>"] = cmp.mapping.complete(),
+
+		-- Navigate between snippet placeholder
+		["<C-f>"] = cmp_action.luasnip_jump_forward(),
+		["<C-b>"] = cmp_action.luasnip_jump_backward(),
+
+		-- Scroll up and down in the completion documentation
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+	}),
+})
+
+require("rose-pine").setup({
+	variant = "auto", -- auto, main, moon, or dawn
+	dark_variant = "main", -- main, moon, or dawn
+	dim_inactive_windows = false,
+	extend_background_behind_borders = false,
+
+	styles = {
+		bold = true,
+		italic = true,
+		transparency = false,
+	},
+
+	groups = {
+		border = "muted",
+		link = "iris",
+		panel = "surface",
+
+		error = "love",
+		hint = "iris",
+		info = "foam",
+		warn = "gold",
+
+		git_add = "foam",
+		git_change = "rose",
+		git_delete = "love",
+		git_dirty = "rose",
+		git_ignore = "muted",
+		git_merge = "iris",
+		git_rename = "pine",
+		git_stage = "iris",
+		git_text = "rose",
+		git_untracked = "subtle",
+
+		headings = {
+			h1 = "iris",
+			h2 = "foam",
+			h3 = "rose",
+			h4 = "gold",
+			h5 = "pine",
+			h6 = "foam",
+		},
+		-- Alternatively, set all headings at once.
+		-- headings = "subtle",
+	},
+
+	highlight_groups = {
+		-- Comment = { fg = "foam" },
+		-- VertSplit = { fg = "muted", bg = "muted" },
+	},
+
+	before_highlight = function(group, highlight, palette)
+		-- Disable all undercurls
+		-- if highlight.undercurl then
+		--     highlight.undercurl = false
+		-- end
+		--
+		-- Change palette colour
+		-- if highlight.fg == palette.pine then
+		--     highlight.fg = palette.foam
+		-- end
+	end,
+})
+
+vim.cmd("colorscheme rose-pine")
+-- vim.cmd('colorscheme rose-pine-main')
+-- vim.cmd('colorscheme rose-pine-moon')
+-- vim.cmd('colorscheme rose-pine-dawn')
+
+local harpoon = require("harpoon")
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function()
+	harpoon:list():append()
+end)
+
+harpoon:setup({})
+
+-- basic telescope configuration
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+	local file_paths = {}
+	for _, item in ipairs(harpoon_files.items) do
+		table.insert(file_paths, item.value)
+	end
+
+	require("telescope.pickers")
+		.new({}, {
+			prompt_title = "Harpoon",
+			finder = require("telescope.finders").new_table({
+				results = file_paths,
+			}),
+			previewer = conf.file_previewer({}),
+			sorter = conf.generic_sorter({}),
+		})
+		:find()
+end
+
+vim.keymap.set("n", "<C-e>", function()
+	toggle_telescope(harpoon:list())
+end, { desc = "Open harpoon window" })
+
+require("stay-centered").setup()
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "number"
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- Use a sub-list to run only the first available formatter
+		javascript = { { "prettierd", "prettier" } },
+	},
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
+
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.autoindent = true
+vim.o.cursorcolumn = true
+vim.o.cursorline = true
+vim.o.hlsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.colorcolumn = "80"
---
+++ lua/plugins/init.lua
@@ -0,0 +1,11 @@
+return {
+	"folke/which-key.nvim",
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+	"folke/neodev.nvim",
+	"folke/zen-mode.nvim",
+	"ThePrimeagen/vim-be-good",
+	"arnamak/stay-centered.nvim",
+	"mbbill/undotree",
+	"tpope/vim-fugitive",
+	"stevearc/conform.nvim",
+}
